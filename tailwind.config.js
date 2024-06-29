acc5a5f89a202910f622b919edeacc3277c923cb
Kerolos Fayez <57182650+kero1019@users.noreply.github.com>
2024-06-29 20:47:18 +0300

    update intializing files

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,21 @@ export default {
   ],
   theme: {
     extend: {
-      colors:{}
+      colors:{
+        "black":"000000",
+        "white":"#FFFFFF",
+        "main-background":"#FFF7ED",
+        "light-beige":"#FFEAD0",
+        "gray-text":"#4D4D4D",
+        "orange":"#FF8C38",
+        "footer-background":"#252525",
+        "footer-text":"#AAAAAA",
+        "normal-text":"#161616",
+        "light-orange":"#FFCC8D",
+        "orange-button":"#E17654",
+        "black-button":"#161616",
+        "green-button":"#115E59",
+      }
     },
   },
   plugins: [],

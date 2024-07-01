1c80aa661931fb1ebf3cdd30a38b47dbe3f9756d
Kerolos Fayez <57182650+kero1019@users.noreply.github.com>
2024-07-01 22:02:53 +0300

    updating header and routes

--- src/App.jsx
+++ src/App.jsx
@@ -4,31 +4,20 @@ import Home from "./Components/Home";
 import About from "./Components/About";
 import Vans from "./Components/Vans";
 import VanDetails from "./Components/VanDetails";
-import "./Server"
+import Layout from "./Components/Layout";
+import "./Server";
 
 function App() {
   return (
-    <div className=" h-screen">
+    <div className="">
       <BrowserRouter>
-        <header className="bg-main-background flex justify-between p-5 items-center h-[15%]">
-          <Link
-            // to="/Components/Home.jsx"
-            to="/VanLife/"
-            className=" font-extrabold text-[2rem]"
-          >
-            #VANLIFE
-          </Link>
-          <nav className="flex  gap-5 text-gray-text font-semibold">
-            <Link to="/VanLife/about">About</Link>
-            <Link to="/VanLife/vans">Vans</Link>
-          </nav>
-        </header>
         <Routes>
-          {/* <Route path="/Components/Home.jsx" element={<Home />} /> */}
-          <Route path="/VanLife/" element={<Home />} />
-          <Route path="/VanLife/about" element={<About />} />
-          <Route path="VanLife/vans" element={<Vans />} />
-          <Route path="VanLife/vanDetails/:id" element={<VanDetails />} />
+          <Route element={<Layout />}>
+            <Route path="/VanLife/" element={<Home />} />
+            <Route path="/VanLife/about" element={<About />} />
+            <Route path="VanLife/vans" element={<Vans />} />
+            <Route path="VanLife/vanDetails/:id" element={<VanDetails />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/Components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Link } from "react-router-dom";
+export default function Header() {
+  return (
+    <header className="bg-main-background flex justify-between p-5 items-center h-[15%]">
+      <Link to="/VanLife/" className=" font-extrabold text-[2rem]">
+        #VANLIFE
+      </Link>
+      <nav className="flex  gap-5 text-gray-text font-semibold">
+        <Link to="/VanLife/about">About</Link>
+        <Link to="/VanLife/vans">Vans</Link>
+      </nav>
+    </header>
+  );
+}

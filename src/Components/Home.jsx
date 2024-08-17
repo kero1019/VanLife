6532294a33d8e4c45c08d366d50b300f45557877
Kerolos Fayez <57182650+kero1019@users.noreply.github.com>
2024-08-17 22:40:08 +0300

    update 17/8/2024

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,13 +1,15 @@
 import React from "react";
-import Footer from "./Footer";
 import OrangeButton from "./OrangeButton";
+import { NavLink } from "react-router-dom";
 export default function Home() {
   return (
     <div className="home-content flex-1 bg-green-900 home">
       <div className="image h-full text-white p-6 flex flex-col justify-around">
         <p className="text-[3rem] font-bold ">You got the travel plans, we got the travel vans.</p>
         <p className="">Add adventure to your life by joining the #vanlife movement. Rent the perfect van to make your perfect road trip.</p>
+        <NavLink  to="/VanLife/vans">
         <OrangeButton>Find your Van</OrangeButton>
+        </NavLink>
       </div> 
     </div>
   );
